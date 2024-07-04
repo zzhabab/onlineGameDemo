12c2e27d8ce45e4efb7df80973713da4ea76a356
zzh <769367712@qq.com>
2024-07-04 19:08:04 +0800

    '1'

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,14 @@
 <template>
-  <!-- 设定视口高度 -->
+  <!-- <div class="box" style="max-height: 500px; overflow: auto;">
+    <div class="container" style="position: relative;" :style="`height: ${myList.length * itemHeight}px;`">
+      <div class="transitionBox" style="width: 100%; position: absolute;">
+        <div class="boxItem" style="width: 100%; height: 100px; text-align: center;" :style="parseInt(item) % 2 === 0 ? 'background-color: lightgray;' : ''" v-for="(item, index) in myList.slice(startIndex, startIndex + count)">
+          {{ item }}
+        </div>
+      </div>
+    </div>
+  </div> -->
   <div class="box" style="max-height: 500px; overflow: auto;">
-    <!-- 设定myList全展示时的height来获得一个真实的scrollBar -->
     <div class="container" style="position: relative;" :style="`height: ${myList.length * itemHeight}px;`">
       <div class="transitionBox" style="width: 100%; position: absolute;">
         <div class="boxItem" style="width: 100%; height: 100px; text-align: center;" :style="parseInt(item) % 2 === 0 ? 'background-color: lightgray;' : ''" v-for="(item, index) in myList.slice(startIndex, startIndex + count)">
@@ -25,27 +32,27 @@ import {
   let lastModuloResult = 0
   const itemHeight = 100
   
-  onMounted(() => {
-    const boxElement = document.querySelector('.box')
-    const transitionBoxElement = document.querySelector('.transitionBox')
-    boxElement?.addEventListener('scroll', (event) => {
-      const currentScrollTop = event.target.scrollTop
-      const moduloResult = Math.min(parseInt(currentScrollTop / itemHeight), myList.length - count)
-      // 往下滚
-      if (moduloResult > lastModuloResult) {
-        startIndex.value = Math.min(startIndex.value + (moduloResult - lastModuloResult), myList.length)
-        transitionBoxElement.style.top = currentScrollTop + 'px'
-        lastModuloResult = moduloResult
-      }
-      // 往上滚
-      if (moduloResult < lastModuloResult) {
-        startIndex.value = Math.max(startIndex.value - (lastModuloResult - moduloResult), 0)
-        // 三元表达式的赋值为0px很有必要，可以调节top值的误差
-        transitionBoxElement.style.top = moduloResult === 0 ? '0px' : currentScrollTop - itemHeight + 'px'
-        lastModuloResult = moduloResult
-      }
-    })
-  })
+  // onMounted(() => {
+  //   const boxElement = document.querySelector('.box')
+  //   const transitionBoxElement = document.querySelector('.transitionBox')
+  //   boxElement?.addEventListener('scroll', (event) => {
+  //     const currentScrollTop = event.target.scrollTop
+  //     const moduloResult = Math.min(parseInt(currentScrollTop / itemHeight), myList.length - count)
+  //     // 往下滚
+  //     if (moduloResult > lastModuloResult) {
+  //       startIndex.value = Math.min(startIndex.value + (moduloResult - lastModuloResult), myList.length)
+  //       transitionBoxElement.style.top = currentScrollTop + 'px'
+  //       lastModuloResult = moduloResult
+  //     }
+  //     // 往上滚
+  //     if (moduloResult < lastModuloResult) {
+  //       startIndex.value = Math.max(startIndex.value - (lastModuloResult - moduloResult), 0)
+  //       // 三元表达式的赋值为0px很有必要，可以调节top值的误差
+  //       transitionBoxElement.style.top = moduloResult === 0 ? '0px' : currentScrollTop - itemHeight + 'px'
+  //       lastModuloResult = moduloResult
+  //     }
+  //   })
+  // })
 </script>
 
 <style>
