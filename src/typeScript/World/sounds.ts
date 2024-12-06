cdb1e5936242b014d627176cba37947c3a2b5340
zzh <769367712@qq.com>
2024-12-06 12:22:52 +0800

    ignorecase set false

---
+++ src/typeScript/World/sounds.ts
@@ -0,0 +1,39 @@
+// import { Howl, Howler } from 'howler'
+
+// import revealSound from '../../sounds/reveal/reveal-1.mp3'
+
+// import engineSound from '../../sounds/engines/1/low_off.mp3'
+
+// import brick1Sound from '../../sounds/bricks/brick-1.mp3'
+// import brick2Sound from '../../sounds/bricks/brick-2.mp3'
+// // import brick3Sound from '../../sounds/bricks/brick-3.mp3'
+// import brick4Sound from '../../sounds/bricks/brick-4.mp3'
+// // import brick5Sound from '../../sounds/bricks/brick-5.mp3'
+// import brick6Sound from '../../sounds/bricks/brick-6.mp3'
+// import brick7Sound from '../../sounds/bricks/brick-7.mp3'
+// import brick8Sound from '../../sounds/bricks/brick-8.mp3'
+
+// import bowlingPin1Sound from '../../sounds/bowling/pin-1.mp3'
+
+// import carHit1Sound from '../../sounds/car-hits/car-hit-1.mp3'
+// // import carHit2Sound from '../../sounds/car-hits/car-hit-2.mp3'
+// import carHit3Sound from '../../sounds/car-hits/car-hit-3.mp3'
+// import carHit4Sound from '../../sounds/car-hits/car-hit-4.mp3'
+// import carHit5Sound from '../../sounds/car-hits/car-hit-5.mp3'
+
+// import woodHit1Sound from '../../sounds/wood-hits/wood-hit-1.mp3'
+
+// import screech1Sound from '../../sounds/screeches/screech-1.mp3'
+
+// import uiArea1Sound from '../../sounds/ui/area-1.mp3'
+
+// import carHorn1Sound from '../../sounds/car-horns/car-horn-1.mp3'
+// import carHorn2Sound from '../../sounds/car-horns/car-horn-2.mp3'
+
+// import horn1Sound from '../../sounds/horns/horn-1.mp3'
+// import horn2Sound from '../../sounds/horns/horn-2.mp3'
+// import horn3Sound from '../../sounds/horns/horn-3.mp3'
+
+// export default class Sounds {
+  
+// }
