a143b2447062bb69aac0e5fee7c9162848722344
zzh <769367712@qq.com>
2024-12-07 09:29:37 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: 'src/models',
+  publicDir: 'public',
   plugins: [vue()],
   resolve: {
     alias: {
